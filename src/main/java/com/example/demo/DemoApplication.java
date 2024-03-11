20dad0491f0384d6dbcb65d604d55f518526b090
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-11 10:44:41 +0900

    feat: 서버 설계 시, 한국 표준시로 데이터가 CRUD 수행 되도록 설정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
@@ -11,6 +13,9 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
+
+        // 한국 표준시(KST) 로 CRUD 작업 되도록 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
         // 메모리 사용량 출력
         long heapSize = Runtime.getRuntime().totalMemory();
