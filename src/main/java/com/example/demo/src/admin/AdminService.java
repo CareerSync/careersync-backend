0486388e7f570013250309564a8c6b1964e2fcf4
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-13 15:36:29 +0900

    feat: add admin controller and service

---
+++ src/main/java/com/example/demo/src/admin/AdminService.java
@@ -0,0 +1,11 @@
+package com.example.demo.src.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class AdminService {
+}
