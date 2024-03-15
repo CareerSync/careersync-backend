46853f476be3b6526702842c2a1a3aff3c4fd2db
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-16 03:21:46 +0900

    fix: modify Payment Entity
    Paymenet 엔티티가 impUid랑 merchantUid 가지도록 수정

--- src/main/java/com/example/demo/src/payment/entity/Payment.java
+++ src/main/java/com/example/demo/src/payment/entity/Payment.java
@@ -1,11 +1,11 @@
 package com.example.demo.src.payment.entity;
 
 import com.example.demo.common.entity.BaseEntity;
-import com.example.demo.src.service.entity.Service;
-import com.example.demo.src.subscription.entity.Subscription;
+import com.example.demo.src.service.entity.Item;
 import com.example.demo.src.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,14 +32,30 @@ public class Payment extends BaseEntity {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "serviceId")
-    private Service service;
+    private Item item;
+
+    @Column(nullable = false)
+    private String impUid;
+
+    @Column(nullable = false)
+    private String merchantUid;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "state", nullable = false, length = 10)
-    private State state;
+    @Column(nullable = false, length = 10)
+    private PaymentState paymentState;
 
-    public enum State {
+    public enum PaymentState {
         SUCCESS, FAIL;
+    }
+
+    @Builder
+    public Payment(Long id, User user, Item item, String impUid, String merchantUid, PaymentState paymentState) {
+        this.id = id;
+        this.user = user;
+        this.item = item;
+        this.impUid = impUid;
+        this.merchantUid = merchantUid;
+        this.paymentState = paymentState;
     }
 
 }
