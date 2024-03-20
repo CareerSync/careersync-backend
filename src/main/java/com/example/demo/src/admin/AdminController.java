97bb5e2a88d4d82a33e45eb332640d5da96b76a9
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-21 00:16:30 +0900

    fix: modify getUserHistory into AdminController
    admin 계정으로 유저 CRUD 히스토리 조회하는 것으로 변경

--- src/main/java/com/example/demo/src/admin/AdminController.java
+++ src/main/java/com/example/demo/src/admin/AdminController.java
@@ -1,12 +1,20 @@
 package com.example.demo.src.admin;
 
 import com.example.demo.common.response.BaseResponse;
+import com.example.demo.src.feed.FeedService;
+import com.example.demo.src.payment.PaymentService;
+import com.example.demo.src.report.ReportService;
 import com.example.demo.src.report.model.PostReportReq;
 import com.example.demo.src.report.model.PostReportRes;
+import com.example.demo.src.user.UserService;
+import com.example.demo.src.user.model.GetUserLogRes;
+import com.example.demo.src.user.model.PostUserLogTimeReq;
 import com.example.demo.utils.MessageUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,6 +23,10 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     private final AdminService adminService;
+    private final UserService userService;
+    private final FeedService feedService;
+    private final ReportService reportService;
+    private final PaymentService paymentService;
     private final MessageUtils messageUtils;
 
     /**
@@ -29,4 +41,45 @@ public class AdminController {
         adminService.blockReportedUsers();
         return new BaseResponse<>("신고된 유저 차단 완료", messageUtils.getMessage("SUCCESS"));
     }
+
+    /**
+     * 회원 CUD 히스토리 전체 조회
+     * [GET] /app/users/history
+     *
+     * 회원 CUD 히스토리 선택 조회
+     * [GET] /app/admin/history/users? revType=
+     * revType 종류
+     * - Create: INSERT
+     * - Update: UPDATE
+     * - Delete: DELETE
+     * @return BaseResponse<List<GetUserLogRes>>
+     */
+    // Path-variable
+    @ResponseBody
+    @GetMapping("/history/users")
+    public BaseResponse<List<GetUserLogRes>> getUserHistory(@RequestParam(required = false) String revType) {
+
+        if (revType == null) {
+            List<GetUserLogRes> getUserHistoryList = userService.getUserHistory();
+            return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
+        }
+
+        List<GetUserLogRes> getUserHistoryList = userService.getUserHistoryByRevType(revType);
+        return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
+    }
+
+    /**
+     * 회원 CUD 히스토리 시간 기준 조회
+     * [POST] /app/admin/history/time/users
+     @return BaseResponse<List<GetUserLogRes>>
+     */
+    // Path-variable
+    @ResponseBody
+    @PostMapping("/history/time/users")
+    public BaseResponse<List<GetUserLogRes>> getUserHistoryByTime(@RequestBody PostUserLogTimeReq req) {
+
+        List<GetUserLogRes> getUserHistoryList = userService.getUserHistoryByTime(req);
+        return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
+    }
+
 }
--- src/main/java/com/example/demo/src/user/UserController.java
+++ src/main/java/com/example/demo/src/user/UserController.java
@@ -99,47 +99,6 @@ public class UserController {
     }
 
     /**
-     * 회원 CUD 히스토리 전체 조회
-     * [GET] /app/users/history
-     *
-     * 회원 CUD 히스토리 선택 조회
-     * [GET] /app/users/history? revType=
-     * revType 종류
-     * - Create: INSERT
-     * - Update: UPDATE
-     * - Delete: DELETE
-     * @return BaseResponse<List<GetUserLogRes>>
-     */
-    // Path-variable
-    @ResponseBody
-    @GetMapping("/log/history")
-    public BaseResponse<List<GetUserLogRes>> getUserHistory(@RequestParam(required = false) String revType) {
-
-        if (revType == null) {
-            List<GetUserLogRes> getUserHistoryList = userService.getUserHistory();
-            return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
-        }
-
-        List<GetUserLogRes> getUserHistoryList = userService.getUserHistoryByRevType(revType);
-        return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
-    }
-
-    /**
-     * 회원 CUD 히스토리 시간 기준 조회
-     * [POST] /app/users/history/time
-     @return BaseResponse<List<GetUserLogRes>>
-     */
-    // Path-variable
-    @ResponseBody
-    @PostMapping("/log/history/time")
-    public BaseResponse<List<GetUserLogRes>> getUserHistoryByTime(@RequestBody PostUserLogTimeReq req) {
-
-        List<GetUserLogRes> getUserHistoryList = userService.getUserHistoryByTime(req);
-        return new BaseResponse<>(getUserHistoryList, messageUtils.getMessage("SUCCESS"));
-    }
-
-
-    /**
      * 유저정보변경 API
      * [PATCH] /app/users
      * RequestBody: PatchUserReq
