29364726a360710e27c9c18f013845b1f8353826
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-07-22 01:24:41 +0900

    fix: modify LoginCheckInterceptor to throw UNAUHTORIZED_USER error

--- src/main/java/com/example/demo/common/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/example/demo/common/interceptor/LoginCheckInterceptor.java
@@ -1,16 +1,17 @@
 package com.example.demo.common.interceptor;
 
-import com.example.demo.common.SessionService;
+import com.example.demo.utils.SessionService;
+import com.example.demo.common.exceptions.BaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import static com.example.demo.common.Constant.LOGIN_MEMBER;
+import static com.example.demo.common.response.BaseResponseStatus.UNAUTHORIZED_USER;
 
 @Slf4j
 @Component
@@ -27,8 +28,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         HttpSession session = sessionService.getSessionFromCookie(request);
 
         if (session == null || session.getAttribute(LOGIN_MEMBER) == null) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인 되지 않은 유저입니다");
-            return false;
+            throw new BaseException(UNAUTHORIZED_USER);
         }
 
         return true;
