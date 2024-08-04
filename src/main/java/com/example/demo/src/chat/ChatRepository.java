42854cca99a6cfb3ccb34837bc88cf3db12ab454
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-08-04 15:42:33 +0900

    feat: add ChatRepository

--- src/main/java/com/example/demo/src/chat/ChatRepository.java
+++ src/main/java/com/example/demo/src/chat/ChatRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo.src.chat;
 
+import com.example.demo.common.entity.BaseEntity;
+import com.example.demo.common.entity.BaseEntity.State;
 import com.example.demo.src.chat.entity.Chat;
 import com.example.demo.src.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
-
+    Optional<Chat> findByIdAndState(UUID id, State state);
 }
