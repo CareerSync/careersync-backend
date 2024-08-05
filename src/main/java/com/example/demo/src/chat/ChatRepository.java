4bd6825ec01acf281ea26769b01f7f8d6e9b1162
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-08-05 21:08:34 +0900

    feat: add getChats API

--- src/main/java/com/example/demo/src/chat/ChatRepository.java
+++ src/main/java/com/example/demo/src/chat/ChatRepository.java
@@ -4,11 +4,17 @@ import com.example.demo.common.entity.BaseEntity;
 import com.example.demo.common.entity.BaseEntity.State;
 import com.example.demo.src.chat.entity.Chat;
 import com.example.demo.src.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
     Optional<Chat> findByIdAndState(UUID id, State state);
+    Page<Chat> findByUserAndStateOrderByUpdatedAtDesc(User user, State state, Pageable pageable);
+    Page<Chat> findByUserAndState(User user, State state, Pageable pageable);
+
 }
