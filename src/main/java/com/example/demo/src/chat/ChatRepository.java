2760e13d194681db49c03daa76078e2836086d27
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-08-06 23:48:19 +0900

    feat: add get jobposts from chat api

--- src/main/java/com/example/demo/src/chat/ChatRepository.java
+++ src/main/java/com/example/demo/src/chat/ChatRepository.java
@@ -3,10 +3,13 @@ package com.example.demo.src.chat;
 import com.example.demo.common.entity.BaseEntity;
 import com.example.demo.common.entity.BaseEntity.State;
 import com.example.demo.src.chat.entity.Chat;
+import com.example.demo.src.jobpost.entity.JobPost;
 import com.example.demo.src.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
