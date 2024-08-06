0a3a4045e47e2a21586990665f845aa5557a8cc9
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-08-06 23:47:44 +0900

    feat: use jpql for fetch jobpost from chat

--- src/main/java/com/example/demo/src/jobpost/JobPostRepository.java
+++ src/main/java/com/example/demo/src/jobpost/JobPostRepository.java
@@ -3,7 +3,10 @@ package com.example.demo.src.jobpost;
 import com.example.demo.common.entity.BaseEntity;
 import com.example.demo.src.jobpost.entity.JobPost;
 import com.example.demo.src.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +15,7 @@ import java.util.UUID;
 import static com.example.demo.common.entity.BaseEntity.*;
 
 public interface JobPostRepository extends JpaRepository<JobPost, UUID> {
-    Optional<List<JobPost>> getJobPostsByUserAndState(User user, State state);
+
+    @Query("SELECT jp FROM JobPost jp WHERE jp.user.id = :userId AND jp.answer.chat.id = :chatId")
+    List<JobPost> findTop3ByChatAndUserOrderByCreatedAtDesc(@Param("chatId") UUID chatId, @Param("userId") UUID userId, Pageable pageable);
 }
